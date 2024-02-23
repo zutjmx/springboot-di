8a382377689c821181f906391ded1f80c0233610
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-02-23 16:34:29 -0600

    Se empieza a crear el componente SomeController

---
+++ src/main/java/com/zutjmx/springboot/di/app/springbootdi/controllers/SomeController.java
@@ -0,0 +1,30 @@
+package com.zutjmx.springboot.di.app.springbootdi.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zutjmx.springboot.di.app.springbootdi.models.Producto;
+import com.zutjmx.springboot.di.app.springbootdi.services.ProductoService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductoService productoService = new ProductoService();
+
+    @GetMapping
+    public List<Producto> getAllProductos() {
+        return productoService.findAllProductos();
+    }
+
+    @GetMapping("/{id}")
+    public Producto geByIProducto(@PathVariable Long id) {
+        return productoService.findByIProducto(id);
+    }
+    
+}
