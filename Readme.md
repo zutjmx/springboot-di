60afea9c40f6fb61634fac6daefa3a84fe22d429
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-02-23 11:01:27 -0600

    first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# springboot-di
+Proyecto de ejemplo para mostrar la inyección de dependecias en SpringBoot, para el curso Spring Framework 6 y Spring Boot 3 desde cero a experto 2024.
---
+++ src/test/java/com/zutjmx/springboot/di/app/springbootdi/SpringbootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zutjmx.springboot.di.app.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
