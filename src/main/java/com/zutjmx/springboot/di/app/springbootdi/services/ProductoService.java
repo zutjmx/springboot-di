544f946e1cb364a24230ac18b9f74445cad05b82
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-02-23 15:59:01 -0600

    Se crea el componente ProductoService

---
+++ src/main/java/com/zutjmx/springboot/di/app/springbootdi/services/ProductoService.java
@@ -0,0 +1,28 @@
+package com.zutjmx.springboot.di.app.springbootdi.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.zutjmx.springboot.di.app.springbootdi.models.Producto;
+import com.zutjmx.springboot.di.app.springbootdi.repositories.ProductoRepository;
+
+public class ProductoService {
+
+    private ProductoRepository productoRepository = new ProductoRepository();
+
+    public List<Producto> findAllProductos() {
+        return productoRepository
+        .findAllProductos()
+        .stream()
+        .map(p -> {
+            Double precioConImpuesto = p.getPrecio()*1.16d;
+            p.setPrecio(precioConImpuesto.longValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Producto findByIProducto(Long id) {
+        return productoRepository.findByIdProducto(id);
+    }
+
+}
